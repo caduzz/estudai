c6b79cb487f3ccce8792a5bdbf4bfd2b5c4f9bf3
Joao <joaopedrosouzarodri@gmail.com>
2025-09-05 15:59:23 -0300

    feat: add connection with database

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,16 @@
 import 'dotenv/config';
 import App from './app';
+import databaseConnection from './config/dbConnection';
 
 const PORT = process.env.PORT || 4545;
 
-new App().start(PORT);
+(async () => {
+  try{
+    await databaseConnection();
+    console.log("Conectado ao MongoDB com sucesso!");
+    
+    new App().start(PORT);
+  } catch (err) {
+    console.error("Erro ao inicializar a aplicação:", err);
+  }
+})();
