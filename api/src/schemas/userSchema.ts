648220eae271b622cfa422f7c7133f3f906eb887
caduzz <67341983+caduzz@users.noreply.github.com>
2025-09-10 12:50:38 -0300

    feature/create-user-schema

---
+++ api/src/schemas/userSchema.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+export const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
