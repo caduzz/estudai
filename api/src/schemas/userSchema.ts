4820bd05ad210fa102d28008b2f5e6488e8d50ad
caduzz <67341983+caduzz@users.noreply.github.com>
2025-09-11 17:52:45 -0300

    bugfix-add-id-user

--- api/src/schemas/userSchema.ts
+++ api/src/schemas/userSchema.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 export const UserSchema = new Schema({
+  id: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
