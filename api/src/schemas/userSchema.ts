b0542a45158acf055ec79e175c94c182b52cebef
caduzz <67341983+caduzz@users.noreply.github.com>
2025-09-11 18:41:13 -0300

    reset-commit-user-id

--- api/src/schemas/userSchema.ts
+++ api/src/schemas/userSchema.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 export const UserSchema = new Schema({
-  id: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
